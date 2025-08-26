c9297f63647e88b90742f106824bce65dd589a50
Danilok95432 <danila.dvoretskov@yandexe.ru>
2025-08-26 10:01:52 +0300

    скрытие терминала

--- src/app/router/MainRoutes.tsx
+++ src/app/router/MainRoutes.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from 'src/pages/home-page/HomePage'
-import { PrintPage } from 'src/pages/print-page/print-page'
-import { TerminalPage } from 'src/pages/terminal-page/terminal-page'
 
 export const MainRoutes = () => {
 	return (
 		<Routes>
-			<Route path={'terminal'} element={<TerminalPage />} />
+			{/*
+				<Route path={'terminal'} element={<TerminalPage />} />
 			<Route path={'terminal/print'} element={<PrintPage />} />
+				*/}
 			<Route path={'/'} element={<HomePage />} />
 		</Routes>
 	)
